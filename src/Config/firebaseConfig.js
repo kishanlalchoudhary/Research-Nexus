2aa33d5f7ebfea74dbc52c6c70ce9b725bc07bb1
Kishanlal Choudhary <kisanchoudhary000@gmail.com>
2023-04-27 21:19:06 +0530

    "website hosted"

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKTbYmDVJIi0SNAUqsGxRKWPsfB3Saj0w",
+  authDomain: "research-nexus-511a9.firebaseapp.com",
+  projectId: "research-nexus-511a9",
+  storageBucket: "research-nexus-511a9.appspot.com",
+  messagingSenderId: "497800236187",
+  appId: "1:497800236187:web:20453e60ff44a206620a3e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
