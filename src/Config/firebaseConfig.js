34afe9831e705d62ff429d73c1169d84f9ffe338
Kishanlal Choudhary <kisanchoudhary000@gmail.com>
2023-05-10 15:31:28 +0530

    "hosted login and signup"

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -1,10 +1,11 @@
-
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"
+import {
+  getAuth,
+  setPersistence,
+  browserSessionPersistence,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider } from "firebase/auth";
-
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyAKTbYmDVJIi0SNAUqsGxRKWPsfB3Saj0w",
@@ -20,3 +21,18 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider(app);
+
+setPersistence(auth, browserSessionPersistence)
+  .then(() => {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return signInWithEmailAndPassword(auth, email, password);
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
