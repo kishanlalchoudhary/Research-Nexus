c436dfb4be3c28dbc3fef455e83e94c52531fc56
Prasad <prasaddhondge1007@gmail.com>
2023-05-20 19:32:12 +0530

    contacts firebase added

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,35 @@
-import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Contact.css"
+import "./Contact.css";
+import React, { useState, useEffect,useRef } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../../Config/firebaseConfig";
+import { useNavigate } from "react-router-dom";
 
 export default function Contact() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+
+  const contactsCollectionRef = collection(db, "contacts");
+  let navigate = useNavigate();
+
+  const contacts = async () => {
+    await addDoc(contactsCollectionRef, {
+      firstName,
+      lastName,
+      email,
+      message,
+      id: auth.currentUser.uid,
+    });
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setMessage("");
+    // navigate("/");
+  };
+
   return (
     <>
       <div class="container-fluid aboutUSLanding">
@@ -22,10 +49,14 @@ export default function Contact() {
               <div class="row contact-content m-sm-m m-2">
                 <div class="form-floating mb-3">
                   <input
+                  value={firstName}
                     type="text"
                     class="form-control"
                     id="floatingFirstName"
                     placeholder="name@example.com"
+                    onChange={(event) => {
+                      setFirstName(event.target.value);
+                    }}
                   />
                   <label for="floatingFirstName" class="tw px-4">
                     First Name
@@ -33,10 +64,14 @@ export default function Contact() {
                 </div>
                 <div class="form-floating mb-3">
                   <input
+                  value={lastName}
                     type="text"
                     class="form-control"
                     id="floatingLastName"
                     placeholder="name@example.com"
+                    onChange={(event) => {
+                      setLastName(event.target.value);
+                    }}
                   />
                   <label for="floatingLastName" class="tw px-4">
                     Last Name
@@ -44,10 +79,14 @@ export default function Contact() {
                 </div>
                 <div class="form-floating mb-3">
                   <input
+                  value={email}
                     type="email"
                     class="form-control"
                     id="floatingInput"
                     placeholder="name@example.com"
+                    onChange={(event) => {
+                      setEmail(event.target.value);
+                    }}
                   />
                   <label for="floatingEmail" class="tw px-4">
                     Email address
@@ -58,10 +97,19 @@ export default function Contact() {
                     Message
                   </label>
                   <textarea
+                    value={message}
                     class="form-control"
                     id="floatingMessage"
                     rows="3"
+                    onChange={(event) => {
+                      setMessage(event.target.value);
+                    }}
                   ></textarea>
+                </div>
+                <div className="btnField">
+                  <button type="submit" id="contactbtn" onClick={contacts}>
+                    Submit
+                  </button>
                 </div>
               </div>
             </div>
